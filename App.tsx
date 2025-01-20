9170206610a62caefb0e42945713c7e5eb1ea1a8
Felipe Mignone <dev.felipemignone@gmail.com>
2025-01-20 20:42:19 -0300

    feat(faceDetector): add and config face detector

--- App.tsx
+++ App.tsx
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { useEffect, useState, useRef } from "react";
+import {
+  Frame,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
+import {
+  Face,
+  Camera,
+  FaceDetectionOptions,
+} from "react-native-vision-camera-face-detector";
 
 export default function App() {
+  const [faces, setFaces] = useState<Face[] | null>(null);
+  const faceDetectionOptions = useRef<FaceDetectionOptions>({
+    performanceMode: "fast",
+    autoScale: true,
+    windowWidth: Dimensions.get("window").width,
+    windowHeight: Dimensions.get("window").height,
+  }).current;
+
+  const device = useCameraDevice("front");
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  useEffect(() => {
+    (async () => {
+      await requestPermission();
+    })();
+  }, []);
+
+  function handleFacesDetection(faces: Face[], frame: Frame) {
+    if (faces.length > 0) {
+      setFaces(faces);
+      return;
+    }
+    setFaces(null);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {!!device ? (
+        <Camera
+          isActive={true}
+          style={StyleSheet.absoluteFill}
+          device={device}
+          faceDetectionCallback={handleFacesDetection}
+          faceDetectionOptions={faceDetectionOptions}
+        />
+      ) : (
+        <Text>No Device</Text>
+      )}
+      {faces !== null &&
+        faces.map((face) => (
+          <View
+            key={`face-at-${face.bounds.x}-${face.bounds.y}`}
+            style={{
+              position: "absolute",
+              top: face.bounds.y,
+              left: face.bounds.x,
+              width: face.bounds.width,
+              height: face.bounds.height,
+              borderColor: "red",
+              borderWidth: 2,
+            }}
+          />
+        ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
